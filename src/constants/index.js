7dc28eca2acabdc25d6c8e6400089d98e8f6d795
sagarksp <111245337+sagarksp@users.noreply.github.com>
2026-03-17 11:28:43 +0530

    Update index.js
    experience updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -153,6 +153,22 @@ const technologies = [
 ];
 
 const experiences = [
+    {
+  {
+  title: "Software Developer",
+  company_name: "VaaaN Infra Ltd.",
+  icon: tekisky,
+  iconBg: "#383E56",
+  date: "August 2025 - Present",
+  points: [
+    "Developing and maintaining the ADAS (Advanced Traffic Management System), contributing to intelligent traffic control and real-time data processing modules.",
+    "Built the MVRN System from scratch using C#, enabling seamless re-audit of transactions with full lifecycle management.",
+    "Worked on ATMS (Automatic Toll Management System) handling report generation using RDLC reporting and VMS UI development for dynamic vehicle monitoring.",
+    "Contributed to the CRMS (Customer Relationship Management System) by resolving critical bugs at both the database and code level, improving system stability and data integrity.",
+    "Worked on AMS (Asset Management System) to streamline asset tracking and management workflows.",
+    "Collaborating with cross-functional teams including designers, product managers, and backend engineers to deliver scalable, high-quality software solutions.",
+  ],
+},
   {
     title: "Full-Stack Developer",
     company_name: "Wibro pvt.ltd",
